2e405776631e6da2fd00d701c172c0c5f535ae92
Kurtis Berger <118536145+milkywaysquirreldeer@users.noreply.github.com>
2023-04-30 11:46:45 -0400

    Enable GUI button input, and its corresponding output to the calculator's display panel

--- js/calculator.js
+++ js/calculator.js
@@ -17,20 +17,53 @@ const divide = function (x, y) {
 let firstNumber;
 let operator;
 let secondNumber;
+const display = document.querySelector('.display');
 
 const operate = function(operator, firstNumber, secondNumber) {
   switch(operator) {
     case '+':
-      console.log(add(firstNumber, secondNumber));
+      display.innerText = add(firstNumber, secondNumber);
       break;
     case '-':
-      console.log(subtract(firstNumber, secondNumber));
+      display.innerText = subtract(firstNumber, secondNumber);
       break;
     case '*':
-      console.log(multiply(firstNumber, secondNumber));
+      display.innerText = multiply(firstNumber, secondNumber);
       break;
     case '/':
-      console.log(divide(firstNumber, secondNumber));
+      display.innerText = divide(firstNumber, secondNumber);
   }
 };
 
+let valueFromDisplay;
+
+/* Provides visual feedback upon button presses when no characters will be
+    added to the display */
+const blinkTheDisplay = function() {
+  display.classList.add('blink-display');
+  setTimeout(() => display.classList.remove('blink-display'), 100);
+};
+
+const displayPressedButton = function(character) {
+  const maximumDigits = 8; // Max characters limited by calculator's display
+  if (display.innerText.length < maximumDigits) {
+    display.innerText === '0' ?
+     display.innerText = character : display.innerText += character;
+     valueFromDisplay = Number.parseFloat(display.innerText);
+  } else {
+    blinkTheDisplay();
+  }
+};
+
+const clearDisplay = () => display.innerText = '0';
+
+document.querySelector('.clear-button').addEventListener('click',
+ clearDisplay);
+
+const digitButtons = document.querySelectorAll('.digit-button');
+
+digitButtons.forEach(button => button.addEventListener('click', () =>
+ displayPressedButton(button.dataset.number)));
+
+document.querySelectorAll('.operator-button').forEach(button =>
+ button.addEventListener('click', () => blinkTheDisplay()));
